b9b4b5b290a6ebbb8dcbedd63420b8e577c46244
Pellaeon Lin <nfsmwlin@gmail.com>
2015-12-11 14:11:24 +0800

    Update# API requires admin user

--- statuspage/views.py
+++ statuspage/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from statuspage.models import *
 from statuspage.serializers import *
 from rest_framework import generics, mixins
+from rest_framework.decorators import permission_classes
+from rest_framework.permissions import IsAdminUser
 
 def index(request):
     context = {
@@ -9,11 +11,13 @@ def index(request):
         }
     return render(request, 'overview.html', context)
 
+@permission_classes((IsAdminUser, ))
 class MonitorList(generics.ListCreateAPIView):
     queryset = Monitor.objects.all()
     serializer_class = MonitorSerializer
     #permission_classes = (IsAdminUser,)
 
+@permission_classes((IsAdminUser, ))
 class MonitorDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Monitor.objects.all()
     serializer_class = MonitorSerializer
