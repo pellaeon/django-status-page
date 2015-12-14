311183945c91e31f558952f6a015e15d9da3e625
Pellaeon Lin <nfsmwlin@gmail.com>
2015-12-14 14:15:19 +0800

    Fix# clicking a subitem should not hide following subitems

--- statuspage/static/statuspage/actions.js
+++ statuspage/static/statuspage/actions.js
@@ -1,5 +1,5 @@
 $('document').ready(function() {
-	$('#status > .ui.segment').click(function(e) {
+	$('#status > .ui.segment.nonsecondary').click(function(e) {
 		$(this).nextUntil('.nonsecondary', '.transition').toggleClass('hidden');
 		$(this).nextUntil('.nonsecondary', '.transition').toggleClass('visible');
 	});
